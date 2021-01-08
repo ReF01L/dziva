356f2113e6e3c3003810b4075afea7b136d4bfe9
ReF0iL <salushkin1998@bk.ru>
2021-01-09 02:54:54 +1000

    ad product_list and product_detail pages and view

--- shop/models.py
+++ shop/models.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Имя')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Ссылка')
 
     class Meta:
         ordering = ('name',)
@@ -14,21 +14,29 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=(self.slug,))
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True, verbose_name='Ссылка')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    available = models.BooleanField(default=True, verbose_name='В доступности')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Изменено')
 
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=(self.id, self.slug,))
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+
+def product_list(req, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(req, 'shop/products/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
+
+
+def product_detail(req, _id, product_slug):
+    product = get_object_or_404(Product, id=_id, slug=product_slug, available=True)
+    return render(req, 'shop/products/detail.html', {
+        'product': product
+    })
