70dfff9ef69fc3f820cd3283b7b06807d17789b5
ReF0iL <salushkin1998@bk.ru>
2021-01-09 02:59:33 +1000

    Add cart functions and cart_detail page

--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 
-def product_list(req, category_slug=None):
+def product_list(request, category_slug=None):
+    category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
-    category = None
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(req, 'shop/products/list.html', {
-        'category': category,
-        'categories': categories,
-        'products': products
-    })
+    return render(request, 'shop/products/list.html',
+                  {
+                      'category': category,
+                      'categories': categories,
+                      'products': products
+                  })
 
 
-def product_detail(req, _id, product_slug):
-    product = get_object_or_404(Product, id=_id, slug=product_slug, available=True)
-    return render(req, 'shop/products/detail.html', {
-        'product': product
+def product_detail(request, _id, slug):
+    product = get_object_or_404(Product, id=_id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/products/detail.html', {
+        'product': product,
+        'cart_product_form': cart_product_form
     })
