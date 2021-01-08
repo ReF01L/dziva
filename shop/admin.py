376c7c3d2044b142d6b9de8a8de5ce42e792a977
ReF0iL <salushkin1998@bk.ru>
2021-01-09 01:51:06 +1000

    Add admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'available', 'created', 'updated')
+    list_editable = ('price', 'available')
+    list_filter = ('available', 'created', 'updated')
+    prepopulated_fields = {'slug': ('name',)}
