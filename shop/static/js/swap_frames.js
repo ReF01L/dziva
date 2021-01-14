34a5d57e69fce92b2ea84235de0df166a647c3bf
ref01l <salushkin.ds@students.dvfu.ru>
2021-01-15 06:11:18 +1000

    update swapper

--- shop/static/js/swap_frames.js
+++ shop/static/js/swap_frames.js
@@ -1,3 +1,12 @@
+const shift_down = (elem, i) => {
+    if (i == 0)
+        elem.style.transform = 'translateY(100%)'
+    else if (i > 1)
+        elem.style.transform = `translateY(${-(i - 1) * 100}%)`
+    else
+        elem.style.transform = 'translateY(0)'
+}
+
 (() => {
     titles = Array.from(document.querySelectorAll('.product__content__body__main__header-title'))
     let frames = Array.from(document.querySelectorAll('.product__content__body__main__content'))
@@ -18,16 +27,7 @@
                 shift_down(active_frame, frames.indexOf(active_frame))
             }
         })
+        if (i != 0)
+            shift_down(frames[i], i)
     });
-    frames[2].style.transform = 'translateY(-100%)'
 })()
-
-const shift_down = (elem, i) => {
-    if (i == 0)
-        elem.style.transform = 'translateY(100%)'
-    else if (i > 1)
-        elem.style.transform = `translateY(${-(i - 1) * 100}%)`
-    else
-        elem.style.transform = 'translateY(0)'
-
-}
